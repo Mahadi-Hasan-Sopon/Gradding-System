7fb76c649df089f72fdde4118d8bff289af3575d
Mahadi-Hasan-Sopon <92266526+Mahadi-Hasan-Sopon@users.noreply.github.com>
2022-08-06 16:50:00 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const result = document.getElementById("result");
+
+
+let userInput = prompt("Please Enter Your Point");
+
+if (userInput <= 100 && userInput > 80) {
+    result.innerHTML = "A+"
+} else if (userInput <= 80 && userInput > 70) {
+    result.innerHTML = "A"
+} else if (userInput <= 70 && userInput > 60) {
+    result.innerHTML = "A-"
+} else if (userInput <= 60 && userInput > 50) {
+    result.innerHTML = "B"
+} else if (userInput <= 50 && userInput > 40) {
+    result.innerHTML = "C"
+} else if (userInput <= 40 && userInput >= 33) {
+    result.innerHTML = "D"
+} else if (userInput < 33 && userInput !== "") {
+    result.innerHTML = "Fail"
+} else if(userInput === "") {
+    result.innerHTML = "Please enter your point";
+} else {
+    result.innerHTML = "Invalid Input"
+}
